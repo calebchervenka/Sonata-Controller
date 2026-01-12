1fb1d11f85e182612292c150e634fa0dd68e0073
Caleb Chervenka <calebchervenka@gmail.com>
2026-01-12 09:57:43 -0600

    Simplified backlight sleep logic

--- include/backlight.h
+++ include/backlight.h
@@ -2,7 +2,7 @@
 
 #define BACKLIGHT_PIN 2
 
-#define BACKLIGHT_DURATION 3000000
+#define BACKLIGHT_DURATION_MS 3000000
 
 int64_t time_last_touch_us;
 
@@ -19,25 +19,22 @@ void disable_backlight()
 void backlight_init()
 {
   pinMode(BACKLIGHT_PIN, OUTPUT);
+  delay(200);
   enable_backlight();
   time_last_touch_us = esp_timer_get_time();
 }
 
-int should_sleep(int is_touched)
+int should_sleep()
 {
-  if (is_touched)
-  {
-    time_last_touch_us = esp_timer_get_time();
-    return 0;
-  }
-  else
-    // return 1;
-    return (esp_timer_get_time() - time_last_touch_us) > BACKLIGHT_DURATION;
+  return (esp_timer_get_time() - time_last_touch_us) > BACKLIGHT_DURATION_MS;
 }
 
 void sleep_backlight_if_inactive(int is_touched)
 {
-  if (should_sleep(is_touched))
+  if (is_touched)
+    time_last_touch_us = esp_timer_get_time();
+
+  if (should_sleep())
     disable_backlight();
   else
     enable_backlight();
